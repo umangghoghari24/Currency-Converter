60e8207776790e18a0b290eb299f65a495762d5d
umangghoghari24 <125755486+umangghoghari24@users.noreply.github.com>
2024-01-28 11:51:54 +0530

    CurrencyConverter

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -12,7 +12,7 @@ class _ConverterState extends State<Converter> {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-            child: Text('Currency Converter'),
+          child: Text('Currency Converter'),
         ),
       ),
 
@@ -21,7 +21,7 @@ class _ConverterState extends State<Converter> {
         child: Column(
           children: [
             Container(
-              height: 150,
+              height: 160,
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(5),
@@ -29,19 +29,22 @@ class _ConverterState extends State<Converter> {
                     color: Colors.indigo.withOpacity(0.3),
                     spreadRadius: 2,
                     blurRadius: 4,
-                    offset: Offset(0,3),
-                  )]
+                    offset: Offset(0, 3),
+                  )
+                  ]
               ),
               child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
                       children: [
                         ClipRRect(
                           borderRadius: BorderRadius.circular(5),
-                          child: Image.network('https://c8.alamy.com/comp/ERNY24/usa-map-in-blue-and-red-colors-ERNY24.jpg',
-                          height: 30,
+                          child: Image.network(
+                            'https://c8.alamy.com/comp/ERNY24/usa-map-in-blue-and-red-colors-ERNY24.jpg',
+                            height: 30,
                             width: 50,
                           ),
                         ),
@@ -50,38 +53,172 @@ class _ConverterState extends State<Converter> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text('USA',style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold
+                              Text('USA', style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold
                               ),),
-                              Text('USD',style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold
+                              Text('USD', style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
                               ),),
                             ],
                           ),
                         ),
                         Icon(Icons.arrow_right,
-                        color: Colors.grey,
+                          color: Colors.grey,
                         )
                       ],
                     ),
                     TextFormField(
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500
+                      ),
                       decoration: InputDecoration(
-                        hintText: '0.0',
-                        suffixIcon: Text('\$',style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey
-                        ),),
-                        suffixIconConstraints: BoxConstraints(
-                          maxWidth: 0,maxHeight: 0
-                        )
+                          hintText: '0.0',
+                          suffixIcon: Text('\$', style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey
+                          ),),
+                          suffixIconConstraints: BoxConstraints(
+                              maxWidth: 0, maxHeight: 0
+                          )
                       ),
                     )
                   ],
                 ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 50, width: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(2),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.indigo.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        )
+                      ]
+                  ),
+                  child: Center(
+                    child: Text(' = ', style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  // width: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.indigo[50],
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                          color: Colors.indigo
+                      )
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Image.network(
+                          'https://c8.alamy.com/comp/ERNY24/usa-map-in-blue-and-red-colors-ERNY24.jpg',
+                          height: 30,),
+                        Text('Switch Currencies', style: TextStyle(
+                            color: Colors.indigo,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500
+                        ),)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30,),
+            _BuildCurrencyView(),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _BuildCurrencyView() {
+    return Container(
+      height: 160,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5),
+          boxShadow: [ BoxShadow(
+            color: Colors.indigo.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 3),
+          )
+          ]
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network(
+                    'https://c8.alamy.com/comp/ERNY24/usa-map-in-blue-and-red-colors-ERNY24.jpg',
+                    height: 30,
+                    width: 50,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('USA', style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold
+                      ),),
+                      Text('USD', style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                      ),),
+                    ],
+                  ),
+                ),
+                Icon(Icons.arrow_right,
+                  color: Colors.grey,
+                )
+              ],
+            ),
+            TextFormField(
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500
+              ),
+              decoration: InputDecoration(
+                  hintText: '0.0',
+                  suffixIcon: Text('\$', style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey
+                  ),),
+                  suffixIconConstraints: BoxConstraints(
+                      maxWidth: 0, maxHeight: 0
+                  )
               ),
             )
           ],
